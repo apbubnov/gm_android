c4245dfde3c9963eced5d9bfe8c6ef23a3c89be5
Dmitry Vasiliev <ceh4top@gmail.com>
2018-01-26 11:50:53 +0300

    Тестовая проверка

--- app/src/main/java/ru/ejevikaapp/authorization/Activity_add_brigade.java
+++ app/src/main/java/ru/ejevikaapp/authorization/Activity_add_brigade.java
@@ -19,7 +19,7 @@ public class Activity_add_brigade extends AppCompatActivity implements View.OnCl
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_add_brigade);
+        //setContentView(R.layout.activity_add_brigade);
 
         name = (EditText)findViewById(R.id.name);
         fio = (EditText)findViewById(R.id.fio);
